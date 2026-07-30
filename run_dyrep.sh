0562297ad4fc62a4601c53c269d536bff268a19e
triptesh1212 <biswastriptesh@gmail.com>
2026-07-30 10:18:01 +0100

    added script for dyrep model

---
+++ run_dyrep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=dyrep
+#SBATCH --account=IscrC_SISTER
+#SBATCH --partition=boost_usr_prod
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2
+#SBATCH --time=10:00:00
+#SBATCH --output=log/slurm_%j.out
+#SBATCH --error=log/slurm_%j.err
+
+module load python/3.11.7
+module load cuda/12.2
+source $WORK/tripteshb/venv/bin/activate
+
+cd $WORK/tripteshb/SL-neural-network-for-temporal-graph
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+python3 -u models/dyrep.py -d wikipedia --gpu 0 --prefix dyrep
